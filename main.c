6fedcb88134061a3d24c172917cf7eaa21ae9f57
jmptable <jmptable@mit.edu>
2013-04-23 00:23:02 -0400

    Simple dumb bluetooth-controlled terminal.

--- main.c
+++ main.c
@@ -28,51 +28,22 @@ void boot(void) {
 int main(void) {
 	boot();
 	
-	screen_print_string("hi world");
-	screen_newline();
-	for(int i=0; i<15; i++)
-		screen_print_char(disk_read(i));
-
-	/*for(;;) {
-		screen_set_pos(0, 0);
-		screen_print_hex(adc_read(0));
-		screen_print_char(' ');
-		screen_print_hex(adc_read(1));
-	}*/
-
 	screen_clear(0x00);
 
-	unsigned char last_level1 = 0;
-	unsigned char last_level2 = 0;
     for(;;) {
-		unsigned int level1 = adc_read(0)>>3; if(level1>=84) level1 = 83;
-		unsigned int level2 = adc_read(1)>>3; if(level2>=84) level2 = 83;
-
-		//clear
-		screen_draw_pos(level1, 0);
-		for(unsigned char i=level1; i<last_level1; i++)
-			screen_draw_slice(0);
-
-		screen_draw_pos(level2, 2);
-		for(unsigned char i=level2; i<last_level2; i++)
-			screen_draw_slice(0);
-
-		//draw
-		screen_draw_pos(0, 0);
-		for(unsigned char i=0; i<level1; i++)
-			screen_draw_slice(0xFF);
-
-		screen_draw_pos(0, 2);
-		for(unsigned char i=0; i<level2; i++)
-			screen_draw_slice(0xFF);
-
-		last_level1 = level1;
-		last_level2 = level2;
-
-		/*while(uart_available()) {
-			char c = uart_read_buff();
-			screen_print_char(c);
-		}*/
+		while(uart_available()) {
+			switch(c) {
+				case 13:	//carriage return
+					screen_print_newline();
+					break
+				case 12:	//form feed
+					screen_clear(0x00);
+					break;
+				default:
+					char c = uart_read_buff();
+					screen_print_char(c);
+			}
+		}
     }
     
     // Never reached.
